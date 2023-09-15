cf47c273aa82d4c10fe451e11e595399a7651380
Milos Markovic <milos.z.markovic.89@gmail.com>
2023-09-15 22:52:12 +0200

    Updated dependencies.

--- mix.exs
+++ mix.exs
@@ -4,18 +4,20 @@ defmodule UeberauthLinkedin.Mixfile do
   @url "https://github.com/fajarmf/ueberauth_linkedin"
 
   def project do
-    [app: :ueberauth_linkedin,
-     version: "0.3.2",
-     name: "Ueberauth LinkedIn Strategy",
-     elixir: "~> 1.2",
-     package: package(),
-     source_url: @url,
-     homepage_url: @url,
-     description: description(),
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps(),
-     docs: docs()]
+    [
+      app: :ueberauth_linkedin,
+      version: "0.3.2",
+      name: "Ueberauth LinkedIn Strategy",
+      elixir: "~> 1.2",
+      package: package(),
+      source_url: @url,
+      homepage_url: @url,
+      description: description(),
+      build_embedded: Mix.env() == :prod,
+      start_permanent: Mix.env() == :prod,
+      deps: deps(),
+      docs: docs()
+    ]
   end
 
   # Configuration for the OTP application
@@ -38,16 +40,20 @@ defmodule UeberauthLinkedin.Mixfile do
   end
 
   defp deps do
-    [{:ueberauth, "~> 0.3"},
-     {:oauth2, "~> 2.0"},
-     {:earmark, "~> 1.3", only: :dev},
-     {:ex_doc, "~> 0.21", only: :dev}]
+    [
+      {:ueberauth, "~> 0.10.5"},
+      {:oauth2, "~> 2.0"},
+      {:earmark, "~> 1.3", only: :dev},
+      {:ex_doc, "~> 0.21", only: :dev}
+    ]
   end
 
   defp package do
-    [files: ["lib", "mix.exs", "README.md", "LICENSE"],
+    [
+      files: ["lib", "mix.exs", "README.md", "LICENSE"],
       maintainers: ["Fajar Firdaus"],
       licenses: ["MIT"],
-      links: %{"Github": @url}]
+      links: %{Github: @url}
+    ]
   end
 end
